e129290cdb903b37b363f726b2cccc77ae179fbf
神庭幸枝 <le.s.kamba@gmail.com>
2018-06-26 22:05:31 +0900

    section2

--- AndroidProj/QiitaClient/app/src/main/kotlin/my/sample/qiitaclient/MainActivity.kt
+++ AndroidProj/QiitaClient/app/src/main/kotlin/my/sample/qiitaclient/MainActivity.kt
@@ -1,12 +1,27 @@
 package my.sample.qiitaclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import my.sample.qiitaclient.model.Article
+import my.sample.qiitaclient.model.User
+import my.sample.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ArticleViewオブジェクトを作成
+        val articleView = ArticleView(applicationContext)
+
+        // Articleオブジェクトを作成して、ArticleViewにセット
+        articleView.setArticle(Article("123",
+                "Kotlin入門",
+                "http://www.example.com/articles/123",
+                User("456", "たろう", "")))
+
+        // このアクティビティにArticleViewをセット
+        setContentView(articleView)
     }
 }
