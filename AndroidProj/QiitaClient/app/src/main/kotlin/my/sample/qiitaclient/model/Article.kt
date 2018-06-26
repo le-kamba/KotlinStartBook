a3add53f77342106dd74d56b73db0253c664d724
神庭幸枝 <le.s.kamba@gmail.com>
2018-06-26 21:38:49 +0900

    section1(REPLはimportで動かないのでクラス定義ごと貼る)

---
+++ AndroidProj/QiitaClient/app/src/main/kotlin/my/sample/qiitaclient/model/Article.kt
@@ -0,0 +1,6 @@
+package my.sample.qiitaclient.model
+
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
---
+++ AndroidProj/QiitaClient/app/src/main/kotlin/my/sample/qiitaclient/model/User.kt
@@ -0,0 +1,6 @@
+package my.sample.qiitaclient.model
+
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
+
