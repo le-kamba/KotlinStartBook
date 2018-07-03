a7c364109159572f6a3a796ffac3c7c81548d29f
神庭幸枝 <le.s.kamba@gmail.com>
2018-07-03 22:36:56 +0900

    section4

---
+++ AndroidProj/QiitaClient/app/src/main/kotlin/my/sample/qiitaclient/ArticleActivity.kt
@@ -0,0 +1,32 @@
+package my.sample.qiitaclient
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebView
+import my.sample.qiitaclient.model.Article
+import my.sample.qiitaclient.view.ArticleView
+
+class ArticleActivity : AppCompatActivity() {
+
+    companion object {
+        private const val ARTICLE_EXTRA: String = "article"
+
+        fun intent(context: Context, article: Article): Intent =
+                Intent(context, ArticleActivity::class.java)
+                        .putExtra(ARTICLE_EXTRA, article)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+
+        val articleView = findViewById<ArticleView>(R.id.article_view)
+        val webView = findViewById<WebView>(R.id.web_view)
+
+        val article: Article = intent.getParcelableExtra(ARTICLE_EXTRA)
+        articleView.setArticle(article)
+        webView.loadUrl(article.url)
+    }
+}
