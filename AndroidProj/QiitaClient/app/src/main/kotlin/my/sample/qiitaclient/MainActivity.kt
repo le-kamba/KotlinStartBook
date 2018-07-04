4027b52c46938024bb3ba9bd9bd8b27fe847bd04
神庭幸枝 <le.s.kamba@gmail.com>
2018-07-04 22:38:20 +0900

    section1(RxAndroidはv2を指定しています)

--- AndroidProj/QiitaClient/app/src/main/kotlin/my/sample/qiitaclient/MainActivity.kt
+++ AndroidProj/QiitaClient/app/src/main/kotlin/my/sample/qiitaclient/MainActivity.kt
@@ -3,9 +3,15 @@ package my.sample.qiitaclient
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ListView
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
 import my.sample.qiitaclient.adapter.ArticleListAdapter
+import my.sample.qiitaclient.client.ArticleClient
 import my.sample.qiitaclient.model.Article
 import my.sample.qiitaclient.model.User
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +30,15 @@ class MainActivity : AppCompatActivity() {
             val article = listAdapter.articles[position]
             ArticleActivity.intent(this, article).let { startActivity(it) }
         }
+
+        val gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://qiita.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build()
+        val articleClient = retrofit.create(ArticleClient::class.java)
     }
 
     // ダミー記事を生成するメソッド
