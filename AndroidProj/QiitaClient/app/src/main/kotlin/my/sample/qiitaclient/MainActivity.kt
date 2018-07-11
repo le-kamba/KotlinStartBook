99797a19c89aeddbb6c82364f55c8780c5066873
神庭幸枝 <le.s.kamba@gmail.com>
2018-07-12 01:04:20 +0900

    section2-2,3 & ソフトウェアキーボードを閉じる

--- AndroidProj/QiitaClient/app/src/main/kotlin/my/sample/qiitaclient/MainActivity.kt
+++ AndroidProj/QiitaClient/app/src/main/kotlin/my/sample/qiitaclient/MainActivity.kt
@@ -1,12 +1,18 @@
 package my.sample.qiitaclient
 
+import android.content.Context
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.ProgressBar
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
+import com.trello.rxlifecycle2.kotlin.bindToLifecycle
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import my.sample.qiitaclient.adapter.ArticleListAdapter
@@ -16,7 +22,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : RxAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
         val listAdapter = ArticleListAdapter(applicationContext)
 
-        val listView: ListView = findViewById(R.id.list_view) as ListView
+        val listView: ListView = findViewById(R.id.list_view)
         listView.adapter = listAdapter
 
         listView.setOnItemClickListener { parent, view, position, id ->
@@ -43,11 +49,25 @@ class MainActivity : AppCompatActivity() {
 
         val queryEditText = findViewById<EditText>(R.id.query_edit_text)
         val searchButton = findViewById<Button>(R.id.search_button)
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
 
-        searchButton.setOnClickListener { v ->
+        searchButton.setOnClickListener {
+            progressBar.visibility = VISIBLE
+
+            val inputMethodManager: InputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(listView.windowToken,
+                    InputMethodManager.HIDE_NOT_ALWAYS)
+            listView.requestFocus()
+
             articleClient.search(queryEditText.text.toString())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
+                    .doAfterTerminate {
+                        progressBar.visibility = GONE
+
+                    }
+                    .bindToLifecycle(this)
                     .subscribe({ list: List<Article> ->
                         queryEditText.text.clear()
                         listAdapter.articles = list
